8e1aadb9cd6e7ffd3e0e2a7328b345a3f316b881
Your Name <youremail@yourdomain.com>
2023-09-22 09:00:49 +0600

    commit

--- bin/homework2.dart
+++ bin/homework2.dart
@@ -1,5 +1,28 @@
-import 'package:homework2/homework2.dart' as homework2;
+import 'dart:ffi';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${homework2.calculate()}!');
+void main() {
+  Map<String, double> exchangeRates = {
+    'USD': 84.5,
+    'EUR': 98.2,
+    'GBP': 115.7,
+    'JPY': 0.761,
+    'CNY': 13.1,
+    'RUB': 1.14,
+  };
+  print('Курсы обмена:');
+  exchangeRates.forEach((currency, rate) {
+    print('$currency: Покупка - $rate, Продажа - ${rate - 1}');
+  });
+  print('\nВыберите операцию:');
+  print('1. Обмен сомов на другую валюту');
+  print('2. Обмен другой валюты на сомы');
+  int operation = int.parse(stdin.readLineSync()!);
+
+  if (operation == 1) {
+    print('Выберите валюту для обмена:');
+    exchangeRates.keys.forEach((currency) {
+      print(currency);
+    });
+  }
 }
